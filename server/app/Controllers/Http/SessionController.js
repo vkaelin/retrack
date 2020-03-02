fe4fc04a284f709f829032d62ebf584ecdb2b171
Valentin Kaelin <kalanehd@gmail.com>
2020-03-02 22:34:32 +0100

    feat: start using persona

--- server/app/Controllers/Http/SessionController.js
+++ server/app/Controllers/Http/SessionController.js
@@ -1,12 +1,20 @@
 'use strict'
 
+const Persona = use('Persona')
+
 class SessionController {
   async store({ auth, request, response }) {
-    const { uid, password } = request.all()
-    console.log(uid, password)
+    // const { uid, password } = request.all()
+    // console.log(uid, password)
 
-    await auth.logout()
-    const user = await auth.attempt(uid, password)
+    // await auth.logout()
+    // const user = await auth.attempt(uid, password)
+
+    const payload = request.only(['uid', 'password'])
+    console.log(payload)
+
+    const user = await Persona.verify(payload)
+    await auth.login(user)
 
     return user
   }
